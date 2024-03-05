512b4220541bdd282e67df2eed7664b581bfe248
Arua <ryangedwill@gmail.com>
2024-03-05 00:57:23 -0800

    added interpreter subclass (subclasses CustomerUser)

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,10 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class Interpreter(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    num_successful_requests = models.IntegerField()
+    
+    def __str__(self):
+        return "interpreter: " + self.user
