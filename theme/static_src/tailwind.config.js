281db8cda7eafddd579be45a996dbdaadda6cb22
Arua <ryan@gedwill.com>
2024-03-15 15:48:09 -0700

    tailwind is configured with theme

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -42,8 +42,29 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
-    },
+        colors: {
+          'blue' : '#19A1C3',
+          'cadet' : {
+            '100': '#eaeaed',
+            '200': '#c0c1c8',
+            '300': '#818391',
+            '400': '#57596c',
+            '500': '#2D3047',
+            '600': '#242639',
+            '700': '#1b1d2b',
+            '800': '#090a0e',
+          },
+          'ivory' : '#FFFFF0',
+          'rose-quartz': {
+            '400': '#fbefee',
+            '500': '#F7CAC9',
+            '600': '#e79998'
+          }
+        },
+        fontFamily: {
+          serif: ['Alegreya Sans', 'serif'],
+        }
+      },
     plugins: [
         /**
          * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
